06ea038281a4858117dd5c402d13738d12959d44
sekalengrengginang <49896401+sekalengrengginang@users.noreply.github.com>
2024-06-24 08:11:50 +0700

    fix: about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -7,10 +7,16 @@ export default function Profile() {
         <div className="flex justify-center bg-gradient-to-b from-[#0D8292]/60 to-transparent bg-cover bg-center">
           <NavigationBar />
         </div>
-        <div className="w-full h-screen ">
-          <div className="flex flex-col lg:flex-row flex-wrap justify-center gap-10 p-5">
-         {/*  */}
+        <div className="w-full h-screen flex justify-center">
+        <div className="lg:bg-white w-screen lg:w-[190vh] mt-5 lg:h-[90vh] p-10 lg:rounded-2xl shadow-2xl  ">
+          <div className="flex justify-center mt-[25vh]">
+          <img src={"/img/logo_jalanku.jpg"} alt="jalanku_logo" className="flex justify-center object-center"/>
           </div>
+         
+          <h1 className="text-6xl font-bold text-center">Jalanku</h1>
+          <h4 className="text-center">by nandiwaelur project</h4>
+
+        </div>
         </div>
         <Footer />
       </div>
